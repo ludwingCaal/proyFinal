889976f697501c64616418c7f19799bbccbf4bd2
ludwing <ludwing.caal1995@gmail.com>
2019-02-06 10:59:33 -0600

    Se agrego la bienvenida

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bienvenido al curso de C#");
+        }
+    }
+}
